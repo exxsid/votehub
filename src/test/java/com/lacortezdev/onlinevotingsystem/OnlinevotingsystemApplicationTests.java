3a18655795eed1762a08bf5e03ce43421d7fdbb3
exxsid <cortezleoanthony5@gmail.com>
2024-10-28 08:43:22 +0800

    init commit

---
+++ src/test/java/com/lacortezdev/onlinevotingsystem/OnlinevotingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacortezdev.onlinevotingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinevotingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
